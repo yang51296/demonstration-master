7b30e8e088acc01aefab15c125b171b1cdafcb79
yang51296 <15221343512@yeah.net>
2018-05-15 15:50:03 +0800

    4324324324

--- cloud-eureka/src/main/java/com/EurekaStart.java
+++ cloud-eureka/src/main/java/com/EurekaStart.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Created  hp on 2018/5/14.
+ * Created sdfdsafsadf fdsafasdfasdfsahp on 2018/5/14.
  */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaStart {
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaStart.class,args);
+    public static void main(String[] argshgdhghgf) {
+        SpringApplication.run(EurekaStart.class,argshgdhghgf);
     }
 }
