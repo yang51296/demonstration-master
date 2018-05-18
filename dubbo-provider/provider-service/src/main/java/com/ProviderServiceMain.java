e092f788c79b193d38688519959b4692c6f33baf
yang51296 <15221343512@yeah.net>
2018-05-18 10:50:47 +0800

    gai dong

--- dubbo-provider/provider-service/src/main/java/com/ProviderServiceMain.java
+++ dubbo-provider/provider-service/src/main/java/com/ProviderServiceMain.java
@@ -1,13 +1,34 @@
 package com;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by hp on 2018/5/4.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@RequestMapping(value = "/api/rest")
+@RefreshScope
 public class ProviderServiceMain {
+
+    @GetMapping("/name")
+    public String getHost() {
+        return host +":"+ host;
+    }
+
+
+    @Value("${test.name}")
+    String host;
+
+
     public static void main(String[] args) {
         SpringApplication.run(ProviderServiceMain.class);
     }
